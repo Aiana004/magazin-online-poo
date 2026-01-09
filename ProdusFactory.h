2975412ee651803f51635d4d9c9f2285f1bd6426
Aiana004 <yusupovaayjahan@gmail.com>
2026-01-09 13:02:52 +0300

    Add Factory pattern for product creation

---
+++ ProdusFactory.h
@@ -0,0 +1,18 @@
+#ifndef PRODUS_FACTORY_H
+#define PRODUS_FACTORY_H
+
+#include <memory>
+#include <string>
+#include "Produs.h"
+
+class ProdusFactory {
+public:
+    static std::unique_ptr<Produs> creeazaProdus(
+        const std::string& tip,
+        const std::string& nume,
+        double pret,
+        const std::string& extra
+    );
+};
+
+#endif
